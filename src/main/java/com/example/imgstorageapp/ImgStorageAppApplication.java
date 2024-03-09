b44f429f00aae868a5286faa69247b12086ad2a4
Miska Rautiainen <miksa.rautiainen@hotmail.com>
2024-03-09 15:00:48 +0200

    Initial commit

---
+++ src/main/java/com/example/imgstorageapp/ImgStorageAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.imgstorageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgStorageAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImgStorageAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imgstorageapp/ImgStorageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imgstorageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgStorageAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
